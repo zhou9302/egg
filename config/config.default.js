f6af84ae504afff3896e6ca46d56271e00dd5629
周颖 <zhouying@rongshantech.com>
2019-08-13 19:19:23 +0800

    feature:second commit

--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,10 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  exports.security = {
+    csrf: false,
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
