b1ffa15b4f63ecb64a20b8098eba7f11ca1efacb
周颖 <zhouying@rongshantech.com>
2019-08-14 00:28:51 +0800

    feature:second commit

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,15 +1,21 @@
 'use strict';
 const Controller = require('egg').Controller;
-const axios = require('axios');
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
   async list() {
-    const aa = await axios.post('https://www.fenghuangbao.com:13443/ckyl/app/api/financing/getProduct', { channel: '1' });
     const { ctx } = this;
-    ctx.body = { statusCode: '00000000', result: aa.data };
+    console.log(ctx);
+    console.log(ctx.request.body);
+    const result = await ctx.curl('https://www.fenghuangbao.com:13443/ckyl/app/api/financing/getProduct', {
+      method: 'post',
+      contentType: 'json',
+      data: ctx.request.body,
+      dataType: 'json',
+    });
+    ctx.body = result.data;
   }
 }
 
